cdbf72ab99dd750c7e2ed46a3170abbee4e14ac3
francois <francois@home>
2016-01-28 23:24:55 +0100

    loading default values from .properties

--- src/main/java/sample/web/ui/InMemoryLicenceRepository.java
+++ src/main/java/sample/web/ui/InMemoryLicenceRepository.java
@@ -1,14 +1,44 @@
 package sample.web.ui;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Properties;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class InMemoryLicenceRepository implements LicenceRepository {
 
-	private static AtomicLong					counter		= new AtomicLong();
+	private static AtomicLong counter = new AtomicLong();
 
-	private final ConcurrentMap<Long, Licence>	licences	= new ConcurrentHashMap<Long, Licence>();
+	private final ConcurrentMap<Long, Licence> licences = new ConcurrentHashMap<Long, Licence>();
+
+	public InMemoryLicenceRepository() {
+		Properties prop = new Properties();
+		try (InputStream input = this.getClass().getResourceAsStream("/licences.properties")) {
+			prop.load(input);
+		} catch (IOException e) {
+			log.error("", e);
+		}
+		createInitialModel(prop);
+	}
+
+	private void createInitialModel(Properties prop) {
+		List<Licence> licences = prop.entrySet().stream().map(elt -> createDefaultLicence((String) elt.getKey(), (String) elt.getValue())).collect(Collectors.toList());
+		for (Licence lic : licences) {
+			this.licences.put(lic.getId(), lic);
+		}
+	}
+
+	private Licence createDefaultLicence(String key, String cmsCode) {
+		Long id = counter.incrementAndGet();
+		return new Licence(id, key, cmsCode);
+	}
 
 	@Override
 	public Iterable<Licence> findAll() {
--- src/main/java/sample/web/ui/Licence.java
+++ src/main/java/sample/web/ui/Licence.java
@@ -4,19 +4,23 @@ import java.util.Calendar;
 
 import org.hibernate.validator.constraints.NotEmpty;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Licence {
 
-	private Long		id;
+	private Long id;
 
 	@NotEmpty(message = "Message is required.")
-	private String		text;
+	private String text;
 
 	@NotEmpty(message = "Summary is required.")
-	private String		summary;
+	private String summary;
 
-	private Calendar	created	= Calendar.getInstance();
+	private final Calendar created = Calendar.getInstance();
 
 }
